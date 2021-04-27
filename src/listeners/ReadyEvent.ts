4f3bf687a8663f181b07e30b174e00e6e8071e89
Hazmi35 <contact@hzmi.xyz>
2021-04-27 09:27:37 +0700

    feat(ReadyEvent): Use Discord.js Client interval

--- src/listeners/ReadyEvent.ts
+++ src/listeners/ReadyEvent.ts
@@ -7,7 +7,7 @@ export class ReadyEvent extends BaseListener {
         await this.client.user?.setPresence({ activity: { name: this.formatString(this.client.config.presenceData.activities[0]), type: "PLAYING" }, status: this.client.config.presenceData.status[0] });
         this.client.logger.info(this.formatString("{username} is ready to serve {users.size} users on {guilds.size} guilds in " +
         "{textChannels.size} text channels and {voiceChannels.size} voice channels!"));
-        setInterval(async () => {
+        this.client.setInterval(async () => {
             const status = Math.floor(Math.random() * this.client.config.presenceData.status.length);
             const activity = Math.floor(Math.random() * this.client.config.presenceData.activities.length);
             await this.client.user?.setPresence({ activity: { name: this.formatString(this.client.config.presenceData.activities[activity]), type: "PLAYING" }, status: this.client.config.presenceData.status[status] });
